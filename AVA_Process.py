b5f55c0a20507059273e191596e390f7a90fcc97
Steve Frederick <sdfreder@asu.edu>
2017-02-11 22:03:27 +0000

    AVA_Process.py created online with Bitbucket

---
+++ AVA_Process.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import urllib2
+import time
+import csv
+import os
+
+#Request user data. Input from user will be placed into variables
+
+fold = raw_input("What is the name of the folder to store the data?\n")
+path = "/home/pi/" + fold
+new = 'N'
+
+while os.path.exists(path) and new == 'N':
+    new = raw_input("That folder already exists. Use it anyway? Y or N\n")
+    if new == "N":
+        fold = raw_input("What is the name of the folder to store the data?\n")
+        path = "/home/pi/" + fold
+        
+if not os.path.exists(path):
+    os.makedirs(path)
+
+choice = raw_input(
+("For three individual axes values (X,Y,Z) enter A\n"
+"For X-axis values enter X\n"
+"For Y-axis values enter Y\n"
+"For Z-axis values enter Z\n")
+)
+samples = input("How many samples (one minute each)?\n")
+
+wait = raw_input("Do you want a delay before sampling begins Y or N\n")
+
+if wait == 'Y':
+    pause = input("How many minutes to delay?\n")
+    print ('waiting...')
+    for i in range(0, pause):
+        print ('minute ' + str(i+1))
+        delay = urllib2.urlopen("http://192.168.1.1/D")
+        count = delay.read()
+        delay.close()
+
+print ('sampling...')        
+
+for j in range(0, samples):
+    W = 0                   #determines if the data will be written to file
+    if choice == 'A':
+        name = choice + ' ' + str(j) # file name is the type of data and loop number
+        print (name)        #debugging step. displays name of file to be created
+        mkr = urllib2.urlopen("http://192.168.1.1/A")
+        accl = mkr.read()
+        mkr.close()
+        W = 1               #data was recieved so a write will be performed
+    elif choice == 'X':
+        name = choice + ' ' + str(j) # file name is the type of data and loop number
+        print (name)
+        mkr = urllib2.urlopen("http://192.168.1.1/X")
+        accl = mkr.read()
+        mkr.close()
+        W = 1
+    elif choice == 'Y':
+        name = choice + ' ' + str(j) # file name is the type of data and loop number
+        print (name)
+        mkr = urllib2.urlopen("http://192.168.1.1/Y")
+        accl = mkr.read()
+        mkr.close()
+        W = 1
+    elif choice == 'Z':
+        name = choice + ' ' + str(j) # file name is the type of data and loop number
+        print (name)
+        mkr = urllib2.urlopen("http://192.168.1.1/Z")
+        accl = mkr.read()
+        mkr.close()
+        W = 1
+        
+    #if user made a valid choice data will be written to text file    
+
+    if W == 1:
+        filenam = path + '/' + name + '.txt'
+        f = open(filenam,"w")
+        f.write(accl)
+        f.close
+
+#if invalid choice was entered user is notified
+        
+if W == 0:
+    print ("invalid entry")
+
+print ("done reading")          #file has been written
