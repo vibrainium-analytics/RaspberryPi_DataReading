c3311c05d61d60a37f4801fd266cf73548231373
Steve <sdfreder@asu.edu>
2017-02-12 20:39:56 -0500

    This will be the main input output and file managing program

--- AVA_Process.py
+++ AVA_Process.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import urllib.request
-import time
+import datetime
 import csv
 import os
+import shutil
 
 
 print ("Starting...\n")
@@ -12,25 +13,28 @@ input(
      "Press enter when done\n")
     )
 
-# Run sensor self test
+# Run connection test and sensor self test
 p = False
 while p == False:
-    self = urllib.request.urlopen("http://192.168.1.1/S").read()
-    selftest = self.decode('ascii')
-    if 'pass' not in selftest:
+    try:
+        self = urllib.request.urlopen("http://192.168.1.1/S", timeout=30).read()
+        selftest = self.decode('ascii')
+        connect = True
+    except (UnicodeDecodeError, urllib.error.URLError):
+        input(
+            ("Not connected. Reset Wi-Fi connection and try again.\n"
+             "Press enter when done.\n")
+            )
+        connect = False
+    if connect == True:
         if 'fail' in selftest:
             input(
                 ("Self test failed. Check sensor for damage or low battery./n"
-                 "Press enter to exit\n")
+                "Press enter to exit\n")
                 )
             quit()
-        else:
-            input(
-                ("Not connected. Reset Wi-Fi connection and try again.\n"
-                 "Press enter when done.\n")
-                )
-    elif 'pass' in selftest:
-        p = True
+        elif 'pass' in selftest:
+            p = True
 
 # Get vehicle info
 veh = input("Enter the name of the vehicle (ex: Steve's Toyota).\n")
@@ -38,109 +42,108 @@ veh = input("Enter the name of the vehicle (ex: Steve's Toyota).\n")
 # Check for baseline data
 path = "/home/owner/"
 pathI = path + veh + "BI"
+pathAC = path + veh + "ACI"
 pathSS = path + veh + "BSS"
+baseI = False
+baseAC = False
+baseSS = False
+
 if os.path.exists(pathI):
     baseI = True
+if os.path.exists(pathAC):
+    baseAC = True    
 if os.path.exists(pathSS):
     baseSS = True
+    print ('Steady Speed baseline exists')
 
 # Get test type
-test = input(
-    ("Enter type of test to run:\n"
-     "For baseline at idle enter 1\n"
-     "For suspected trouble at idle enter 2\n"
-     "For baseline at steady speed enter 3\n"
-     "For trouble at steady speed enter 4\n")
-    )
+rt = True
+while rt == True:
+    test = input(
+        ("Enter type of test to run:"
+         "For baseline at idle enter 1\n"
+         "For suspected trouble at idle enter 2\n"
+         "For baseline at steady speed enter 3\n"
+         "For trouble at steady speed enter 4\n")
+        )
+    if test == '1':
+        AC = input("Will the AC be on Y or N")
+        if AC == "Y" and baseAC == True:
+            ov = input("Baseline exists. Do you want to overwrite? Y or N")
+            if ov == "Y":
+                shutil.rmtree(pathAC)
+                print("baseline removed")
+                os.makedirs(pathAC)
+                datapath = pathAC
+                rt = False
+            else:
+                rt = True
+        elif AC == "Y":
+            os.makedirs(pathAC)
+            datapath = pathAC
+            rt = False
+        if AC == "N" and baseI == True:
+            ov = input("Baseline exists. Do you want to overwrite? Y or N")
+            if ov == "Y":
+                shutil.rmtree(pathI)
+                print("baseline removed")
+                os.makedirs(pathI)
+                datapath = pathI
+                rt = False
+            else:
+                rt = True
+        elif AC == "N":
+            os.makedirs(pathI)
+            datapath = pathI
+            rt = False
+    if test == '2':
+        now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
+        pathTI = path + veh + " Idle Trouble/" + now
+        os.makedirs(pathTI)
+        datapath = pathTI
+        rt = False
+    if test == '3':
+        SS = True
+        pathtemp = path + 'tempSS/'
+        os.makedirs(pathtemp)
+        datapath = pathtemp
+        rt = False
+    if test == '4':
+        now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
+        pathTSS = path + veh + " Steady Speed Trouble/" + now
+        os.makedirs(pathTSS)
+        datapath = pathTSS
+        rt = False
 
-if test == 1 and baseI == True:
-    ov = input ("Baseline at idle exists for this vehicle. Overwrite? Y or N\n")
+# How long to sample and delay
+samples = int(input("How many samples (one minute each)?\n"))
+wait = input("Do you want a delay before sampling begins Y or N\n")
+if wait == 'Y':
+    pause = int(input("How many minutes to delay?\n"))
+    print ('waiting...')
+    for i in range(0, pause):
+        print ('minute ' + str(i+1))
+        delay = urllib.request.urlopen("http://192.168.1.1/D")
+        count = delay.read()
+        delay.close()
 
-os.makedirs(pathI)
-
-
-    
+print ('sampling...')
+for j in range(0, samples):
+        name = "Three Axes" + ' ' + str(j) # file name is the type of data and loop number
+        print (name)        #debugging step. displays name of file to be created
+        mkr = urllib.request.urlopen("http://192.168.1.1/A")
+        accl = str(mkr.read())
+        mkr.close()
+        filenam = datapath + '/' + name + '.txt'
+        f = open(filenam,"w")
+        f.write(accl)
+        f.close
+print ("done reading")
+        
 
         
-        
-    
 
-##fold = raw_input("What is the name of the folder to store the data?\n")
-##path = "/home/pi/" + fold
-##new = 'N'
-##
-##while os.path.exists(path) and new == 'N':
-##    new = raw_input("That folder already exists. Use it anyway? Y or N\n")
-##    if new == "N":
-##        fold = raw_input("What is the name of the folder to store the data?\n")
-##        path = "/home/pi/" + fold
-##        
-##if not os.path.exists(path):
-##    os.makedirs(path)
-##
-##choice = raw_input(
-##("For three individual axes values (X,Y,Z) enter A\n"
-##"For X-axis values enter X\n"
-##"For Y-axis values enter Y\n"
-##"For Z-axis values enter Z\n")
-##)
-##samples = input("How many samples (one minute each)?\n")
-##
-##wait = raw_input("Do you want a delay before sampling begins Y or N\n")
-##
-##if wait == 'Y':
-##    pause = input("How many minutes to delay?\n")
-##    print ('waiting...')
-##    for i in range(0, pause):
-##        print ('minute ' + str(i+1))
-##        delay = urllib2.urlopen("http://192.168.1.1/D")
-##        count = delay.read()
-##        delay.close()
-##
-##print ('sampling...')        
-##
-##for j in range(0, samples):
-##    W = 0                   #determines if the data will be written to file
-##    if choice == 'A':
-##        name = choice + ' ' + str(j) # file name is the type of data and loop number
-##        print (name)        #debugging step. displays name of file to be created
-##        mkr = urllib2.urlopen("http://192.168.1.1/A")
-##        accl = mkr.read()
-##        mkr.close()
-##        W = 1               #data was recieved so a write will be performed
-##    elif choice == 'X':
-##        name = choice + ' ' + str(j) # file name is the type of data and loop number
-##        print (name)
-##        mkr = urllib2.urlopen("http://192.168.1.1/X")
-##        accl = mkr.read()
-##        mkr.close()
-##        W = 1
-##    elif choice == 'Y':
-##        name = choice + ' ' + str(j) # file name is the type of data and loop number
-##        print (name)
-##        mkr = urllib2.urlopen("http://192.168.1.1/Y")
-##        accl = mkr.read()
-##        mkr.close()
-##        W = 1
-##    elif choice == 'Z':
-##        name = choice + ' ' + str(j) # file name is the type of data and loop number
-##        print (name)
-##        mkr = urllib2.urlopen("http://192.168.1.1/Z")
-##        accl = mkr.read()
-##        mkr.close()
-##        W = 1
-##        
-##    #if user made a valid choice data will be written to text file    
-##
-##    if W == 1:
-##        filenam = path + '/' + name + '.txt'
-##        f = open(filenam,"w")
-##        f.write(accl)
-##        f.close
-##
-###if invalid choice was entered user is notified
-##        
-##if W == 0:
-##    print ("invalid entry")
-##
-##print ("done reading")          #file has been written
+
+
+
+
