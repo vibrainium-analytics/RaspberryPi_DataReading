4ac9d7b67a31844d5d95f8fdb53dfb291ed2009c
Steve <sdfreder@asu.edu>
2017-02-15 14:04:34 -0500

    Adjusted to match flow chart

--- AVA_Process.py
+++ AVA_Process.py
@@ -7,6 +7,10 @@ import os
 import shutil
 
 
+# Initialize Variables
+path = "/home/pi/"
+temp = path + "temp/"
+
 print ("Starting...\n")
 input(
     ("Turn on Wi-fi and connect to wifi101-network.\n"
@@ -14,10 +18,10 @@ input(
     )
 
 # Run connection test and sensor self test
-p = False
-while p == False:
+pasd = False
+while pasd == False:
     try:
-        self = urllib.request.urlopen("http://192.168.1.1/S", timeout=30).read()
+        self = urllib.request.urlopen("http://192.168.1.1/S", timeout=20).read()
         selftest = self.decode('ascii')
         connect = True
     except (UnicodeDecodeError, urllib.error.URLError):
@@ -34,114 +38,147 @@ while p == False:
                 )
             quit()
         elif 'pass' in selftest:
-            p = True
+            pasd = True
 
-# Get vehicle info
-veh = input("Enter the name of the vehicle (ex: Steve's Toyota).\n")
+# Get vehicle name
+veh = input("Enter the name of the vehicle (ex: Steve-Toyota).\n")
 
-# Check for baseline data
-path = "/home/owner/"
-pathI = path + veh + "BI"
-pathAC = path + veh + "ACI"
-pathSS = path + veh + "BSS"
-baseI = False
-baseAC = False
-baseSS = False
-
+# Check for baseline data and report to user
+pathI = path + veh + "-BaseIdle/"
+pathAC = path + veh + "-BaseACIdle/"
+pathSS = path + veh + "-BaseStdySpd/"
 if os.path.exists(pathI):
     baseI = True
+    print('Baseline folder exists for AC off')
+else:
+    print('No standard baseline exists for this vehicle with AC on at idle')
+    baseAC = False    
 if os.path.exists(pathAC):
-    baseAC = True    
+    baseAC = True
+    print('Baseline folder exists for AC on')
+else:
+    print('No standard baseline exists for this vehicle with AC off at idle')
+    baseI = False
 if os.path.exists(pathSS):
     baseSS = True
-    print ('Steady Speed baseline exists')
+    print ('Steady Speed baseline folder exists')
+else:
+    print('No standard baselines exist for this vehicle at steady speed.')
+    baseSS = False
 
-# Get test type
-rt = True
-while rt == True:
-    test = input(
-        ("Enter type of test to run:"
-         "For baseline at idle enter 1\n"
-         "For suspected trouble at idle enter 2\n"
-         "For baseline at steady speed enter 3\n"
-         "For trouble at steady speed enter 4\n")
-        )
-    if test == '1':
-        AC = input("Will the AC be on Y or N")
-        if AC == "Y" and baseAC == True:
-            ov = input("Baseline exists. Do you want to overwrite? Y or N")
-            if ov == "Y":
-                shutil.rmtree(pathAC)
-                print("baseline removed")
-                os.makedirs(pathAC)
-                datapath = pathAC
-                rt = False
-            else:
-                rt = True
-        elif AC == "Y":
-            os.makedirs(pathAC)
-            datapath = pathAC
-            rt = False
-        if AC == "N" and baseI == True:
-            ov = input("Baseline exists. Do you want to overwrite? Y or N")
-            if ov == "Y":
-                shutil.rmtree(pathI)
-                print("baseline removed")
-                os.makedirs(pathI)
-                datapath = pathI
-                rt = False
-            else:
-                rt = True
-        elif AC == "N":
-            os.makedirs(pathI)
-            datapath = pathI
-            rt = False
-    if test == '2':
-        now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
-        pathTI = path + veh + " Idle Trouble/" + now
-        os.makedirs(pathTI)
-        datapath = pathTI
-        rt = False
-    if test == '3':
-        SS = True
-        pathtemp = path + 'tempSS/'
-        os.makedirs(pathtemp)
-        datapath = pathtemp
-        rt = False
-    if test == '4':
-        now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
-        pathTSS = path + veh + " Steady Speed Trouble/" + now
-        os.makedirs(pathTSS)
-        datapath = pathTSS
-        rt = False
+# Get test number
+test = int(input(
+    ("\nEnter type of test to run:\n"
+     "For baseline at idle enter 1\n"
+     "For suspected trouble at idle enter 2\n"
+     "For baseline at steady speed enter 3\n"
+     "For trouble at steady speed enter 4\n")
+    ))
 
-# How long to sample and delay
-samples = int(input("How many samples (one minute each)?\n"))
-wait = input("Do you want a delay before sampling begins Y or N\n")
-if wait == 'Y':
-    pause = int(input("How many minutes to delay?\n"))
-    print ('waiting...')
-    for i in range(0, pause):
-        print ('minute ' + str(i+1))
-        delay = urllib.request.urlopen("http://192.168.1.1/D")
-        count = delay.read()
-        delay.close()
+# Check for delay request and sample length
+pause = int(input(
+    ("\nHow many minutes do you want to delay to allow the vehicle\n"
+     "to reach desired test conditions. (enter 0 for no delay)\n")
+    ))
+samples = int(input("\nHow many one minute samples? (there should be at least 3)\n"))
+print ('waiting...')
+for i in range(0, pause):
+    print ('minute ' + str(i+1))
+    delay = urllib.request.urlopen("http://192.168.1.1/D")
+    count = delay.read()
+    delay.close()
 
+# collect data
 print ('sampling...')
 for j in range(0, samples):
-        name = "Three Axes" + ' ' + str(j) # file name is the type of data and loop number
-        print (name)        #debugging step. displays name of file to be created
-        mkr = urllib.request.urlopen("http://192.168.1.1/A")
-        accl = mkr.read().decode()
-        mkr.close()
-        filenam = datapath + '/' + name + '.txt'
-        f = open(filenam,"w")
-        f.write(accl)
-        f.close
+    name = "Three Axes" + ' ' + str(j+1)
+    num = str(j+1)
+    print ('sample #' + num)                                                
+    mkr = urllib.request.urlopen("http://192.168.1.1/A")
+    accl = mkr.read().decode()
+    mkr.close()
+    os.makedirs(temp)
+    filenam = temp + name + '.txt'
+    f = open(filenam,"w")
+    f.write(accl)
+    f.close
 print ("done reading")
-        
 
-        
+# set up folders for data        
+if test == 1:
+    AC = input("Was the AC on? Y or N")
+    if AC == "Y" or AC == 'y' and baseAC == True:
+        ow = input("Baseline exists. Do you want to overwrite? Y or N")
+        if ow == "Y" or ow == 'y':
+            shutil.rmtree(pathAC)           
+            os.makedirs(pathAC)
+            datapath = pathAC
+            print("baseline replaced")
+        else:
+            new = input("Enter the name for this new baseline.")
+            datapath = pathAC + new + "/"
+            os.makedirs(datapath)
+    elif AC == "Y" or AC == 'y':
+        os.makedirs(pathAC)
+        datapath = pathAC
+    if AC == "N" or AC =="n" and baseI == True:
+        ow = input("Baseline exists. Do you want to overwrite? Y or N")
+        if ow == "Y" or ow == 'y':
+            shutil.rmtree(pathI)
+            os.makedirs(pathI)
+            datapath = pathI
+            print("baseline replaced")
+        else:
+            new = input("Enter the name for this new baseline.")
+            datapath = pathI + new + "/"
+            os.makedirs(datapath)
+    elif AC == "N" or AC == 'n':
+        os.makedirs(pathI)
+        datapath = pathI
+if test == 2:
+    AC = input("Was the AC on? Y or N")
+    if AC == 'Y' or AC == 'y':
+        trb = "-AC-Idle-Trbl/"
+    else:
+        trb = "-Idle-Trbl-NO-AC/"
+    now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
+    pathTI = path + now + veh + trb + '/'
+    os.makedirs(pathTI)
+    datapath = pathTI
+if test == 3:
+    speed = int(input('What was the vehicle speed in miles per hour for the test?'))
+    pathSS1 = pathSS + str(speed) + 'MPH/'
+    if baseSS == True:
+        #check for existing baseline at this speed
+        if os.path.exists(pathSS):
+            ow = input("Baseline exists for this speed. Do you want to overwrite? Y or N")
+            if ow == "Y" or ow == 'y':
+                shutil.rmtree(pathSS1)           
+                os.makedirs(pathSS1)
+                datapath = pathSS1
+                print("baseline replaced")
+            else:
+                new = input("Enter the name for this new baseline.")
+                datapath = pathSS + new + "/"
+                os.makedirs(datapath)
+        else:
+            os.makedirs(pathSS1)
+            datapath = pathSS1 
+    else:
+        os.makedirs(pathSS1)
+        datapath = pathSS1
+if test == 4:
+    speed = int(input('What was the vehicle speed in miles per hour for the test?'))    
+    now = '{:%Y-%b-%d %H:%M}'.format(datetime.datetime.now()) 
+    pathTSS = path + now + veh + '-Stdy-Spd-Trbl' + str(speed) +'MPH/'
+    os.makedirs(pathTSS)
+    datapath = pathTSS
+
+# Limit number of samples 
+
+
+
+    
 
 
 
